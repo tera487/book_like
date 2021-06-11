b197e5b9218e3b091d35521bdd7f9140c13e2eca
tera <tera.1999.kagayaku@ezweb.ne.jp>
2021-06-11 20:49:38 +0900

    [add]User, Admin devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions",
+    registrations: "admins/registrations"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 
