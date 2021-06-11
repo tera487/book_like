c88d33bec1d74e2f55cb65fdf942893084c05e02
tera <tera.1999.kagayaku@ezweb.ne.jp>
2021-06-11 15:56:01 +0900

    [add]route [update]database.yml,layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  private def set_layout
+    if params[:controller].match(%r{\A(user|admin)/})
+      Regexp.last_match[1]
+    else
+      "user"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "home#index"
+
+  namespace :admins do
+    root "posts#index"
+    resources :posts, only: [:index, :destroy] do
+      get :reports_posts, on: :collection
+    end
+    resources :users, only: [:show, :destroy]
+    resources :articles, except: [:show]
+  end
+
+  namespace :users do
+    root "posts#index"
+    resources :posts do
+      resources :nice_posts, only: [:create, :destroy]
+      resources :reports, only: [:create, :destroy]
+    end
+
+    resources :books, only: [:show, :index] do
+      get "search", on: :collection
+    end
+    resources :articles, only: :index
+
+    resources :read_book, only: :show
+
+    resources :users, only: :show do
+      member do
+        get :following, :follower
+      end
+    end
+  end
+
+
+
+
 end
