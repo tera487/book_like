48cb324388a339b6ed5ee6074b2da02167291e66
tera <tera.1999.kagayaku@ezweb.ne.jp>
2021-06-14 12:30:19 +0900

    [add]Top,sign_in,sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   layout :set_layout
 
   private def set_layout
-    if params[:controller].match(%r{\A(user|admin)/})
+    if params[:controller].match(%r{\A(users|admins)/})
       Regexp.last_match[1]
     else
-      "user"
+      "users"
     end
   end
 end
