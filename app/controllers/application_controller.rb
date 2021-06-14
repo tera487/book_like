8f4d8b36c0fdd1f85c139a86857a75cd74388332
tera <tera.1999.kagayaku@ezweb.ne.jp>
2021-06-14 17:45:36 +0900

    [update]sing_up sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
+  # deviseコントローラにストロングパラメータを追加
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # 編集画面から画像を受け取れるよう設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar name))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(avatar name))
+  end
 
   private def set_layout
     if params[:controller].match(%r{\A(users|admins)/})
