817e9baa5a5e85c9acb5fd32b3c8e88ae7c908e4
tera <tera.1999.kagayaku@ezweb.ne.jp>
2021-06-22 19:30:16 +0900

    [add]admin articlesの更新

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -21,6 +21,32 @@ class Admins::ArticlesController < ApplicationController
     end
   end
 
+  def show
+    @articles = Article.find(params[:id])
+    
+  end
+  
+  def edit
+    @articles = Article.find(params[:id])
+  end
+  
+  def update
+    @articles = Article.find(params[:id])
+    @articles.assign_attributes(article_params)
+    if @articles.save
+      redirect_to admins_article_path(@articles)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+    redirect_to :admins_articles
+  end
+
+
   private 
     def article_params
       params.require(:article).permit(:article_title, :image,:article_content)
